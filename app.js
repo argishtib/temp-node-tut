4c23b41951e4c30b8288039314ca259ca59c9594
Argishti Baghdasarian <abaghdasarian@activ247.com>
2023-06-17 22:51:41 -0700

    node setup

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const _ = require("lodash");
+const sqlite3 = require('sqlite3').verbose();
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("sd1");
+// open the database
+let db = new sqlite3.Database('./db/ACTIV_Web_App_DB.db');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    let query = `SELECT COUNT(*) FROM TASK WHERE PROJ_ID = 370`;
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
